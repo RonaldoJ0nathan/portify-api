dfdb4be8fb1a51f4d5935117628f97cc0fc24f5d
Ronaldo Jonathan <ronaldoj.dev@gmail.com>
2026-01-18 17:45:00 -0400

    feat(config): configure global ConfigModule with env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { appConfig, envValidationSchema } from './config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+      validate: (config) => envValidationSchema.parse(config),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
