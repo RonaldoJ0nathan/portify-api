0bff4029353b0db751893b66b171c4273460a349
Ronaldo Jonathan <ronaldoj.dev@gmail.com>
2026-01-18 20:05:12 -0400

    feat(core): enable uri-based api versioning

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -7,6 +7,11 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   app.setGlobalPrefix('api', {
     exclude: ['health', 'metrics', 'docs'],
